a533ca7645c65dcd55a9b3356a09ed2c55e02bf8
Aman Dhiman <amandhiman251@gmail.com>
2020-12-24 17:51:52 +0530

    Switch-Case in the Reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,25 +5,24 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
 
-    if(action.type==="INCREMENT"){
-        return {
-            counter: state.counter+1
-        };
-    }
-    if(action.type==="DECREMENT"){
-        return {
-            counter: state.counter-1
-        };
-    }
-    if(action.type==="ADD5"){
-        return {
-            counter: state.counter+action.val
-        };
-    }
-    if(action.type==="SUBTRACT5"){
-        return {
-            counter: state.counter-action.val
-        };
+    switch(action.type){
+        case "INCREMENT":
+            return {
+                counter: state.counter+1
+            };
+        case "DECREMENT":
+            return {
+                counter: state.counter-1
+            }
+        case "ADD5":
+            return {
+                counter: state.counter+action.val
+            };
+        case "SUBTRACT5":
+            return {
+                counter: state.counter-action.val
+            };
+
     }
     return state
 }
